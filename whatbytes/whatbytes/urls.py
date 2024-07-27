ad778b9e6331e22e21ae639383f83eea1568f4d2
Saumya Srivastava <117256519+SaumyaSrivastava13@users.noreply.github.com>
2024-07-27 12:43:17 +0530

    Update urls.py

--- whatbytes/whatbytes/urls.py
+++ whatbytes/whatbytes/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.shortcuts import redirect
+
+def redirect_to_accounts(request, path=None):
+    return redirect('login')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
+    re_path(r'^.*$', redirect_to_accounts),
 ]
